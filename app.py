83221d27fa70cc285bfe832bb49b351a8d675654
Javad Pournosrat <Pournosrat.javad@gmail.com>
2024-10-01 11:31:40 +0330

    comments

--- app.py
+++ app.py
@@ -4,11 +4,14 @@ import numpy as np
 
 x, y = sp.symbols('x y')
 
+#number of polynomials
 n = m = 40
 
+#Geometrical properties
 a, b = 8, 8
 h = 1
 
+#mechanical properties
 E = 1
 nu = 0.3
 G = E/(2*(1+nu))
@@ -27,4 +30,6 @@ Wmn = 0
 for i in range(1, m):
     for j in range(1, n):
         Wmn += W(i, j)
+
+#Deflection at x=a/2 and y=b/2
 print(Wmn)
