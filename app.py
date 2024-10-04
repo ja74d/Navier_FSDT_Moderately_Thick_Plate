cedf28aabbea0658a7e4c3654f4c5273e71efac5
Javad Pournosrat <Pournosrat.javad@gmail.com>
2024-10-04 16:14:23 +0330

    Non dimentional disp

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import numpy as np
 x, y = sp.symbols('x y')
 
 #number of polynomials
-n = m = 10
+n = m = 20
 
 #Geometrical properties
 a, b = 8, 8
@@ -18,18 +18,22 @@ G = E/(2*(1+nu))
 k = (5/6)**0.5
 D = (E*h**3)/(12*(1-nu**2))
 
+#P0
+p0 = 1
+
 #Pmn
 def P(m, n):
-    return (4/(a*b))*( sp.integrate(sp.sin( (m*math.pi*x)/(a) ), (x, 0, a)) * sp.integrate(sp.sin( (n*math.pi*y)/b ), (y, 0, b)) )
+    return (4/(a*b))*( sp.integrate(p0*sp.sin( (m*math.pi*x)/(a) ), (x, 0, a)) * sp.integrate(sp.sin( (n*math.pi*y)/b ), (y, 0, b)) )
 
 #Wmn
 def W(m, n):
     return ( 1 + ( ((D*(math.pi**2))/(k**2 * G * h))*( (m/a)**2 + (n/b)**2 ) ) ) * ( ( P(m, n) )/( (D*(math.pi**4))*( ( (m/a)**2 + (n/b)**2 )**2 ) ) )
 
 Wmn = 0
-for i in range(1, m):
-    for j in range(1, n):
+for i in range(1, m+1):
+    for j in range(1, n+1):
         Wmn += W(i, j)
 
 #Deflection at x=a/2 and y=b/2
 print(Wmn)
+print(Wmn/( p0 * a**4 / D ))
---
+++ untitled.geo
@@ -0,0 +1,40 @@
+//+
+Point(1) = {8, 8, 0, 1.0};
+//+
+Point(2) = {8, 0, 0, 1.0};
+//+
+Point(3) = {0, 0, 0, 1.0};
+//+
+Point(4) = {0, 8, 0, 1.0};
+//+
+Line(1) = {4, 1};
+//+
+Line(2) = {1, 2};
+//+
+Line(3) = {2, 3};
+//+
+Line(4) = {3, 4};
+//+
+Curve Loop(1) = {4, 1, 2, 3};
+//+
+Plane Surface(1) = {1};
+//+
+Physical Curve("Left", 5) = {4};
+//+
+Physical Curve("Top", 6) = {1};
+//+
+Physical Curve("Right", 7) = {2};
+//+
+Physical Curve("Bottom", 8) = {3};
+//+
+Physical Point("Bottom", 9) = {3, 2};
+//+
+Physical Point("Left", 10) = {3, 4};
+//+
+Physical Point("Top", 11) = {4, 1};
+//+
+Physical Point("Right", 12) = {1, 2};
+//+
+Transfinite Surface {1} = {4, 1, 2, 3};
+//+
+Recombine Surface {1};
